dbf33c31ec265e776e763fdc4e5d856be01b5fb6
Tomasz <50521081+IDontCareKappa@users.noreply.github.com>
2022-04-21 14:19:52 +0200

    Lab_8

---
+++ Lab_8/server/index.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+
+const userRoutes = require("./routes/users")
+const authRoutes = require("./routes/auth")
+
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const port = process.env.PORT || 8080
+
+const connection = require('./db')
+connection()
+
+app.use(express.json())
+app.use(cors())
+
+// routes
+app.use("/api/users", userRoutes)
+app.use("/api/auth", authRoutes)
+
+app.listen(port, () => console.log(`Nasłuchiwanie na porcie ${port}`))
