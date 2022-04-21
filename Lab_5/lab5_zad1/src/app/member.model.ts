0df927fc0b02f20c573a51a7d45bfbc204fdf049
Tomasz <50521081+IDontCareKappa@users.noreply.github.com>
2022-04-21 11:49:36 +0200

    Lab_5

---
+++ Lab_5/lab5_zad1/src/app/member.model.ts
@@ -0,0 +1,12 @@
+export class Member {
+    name: string;
+    mail: string;
+    membershipDate: Date;
+    membershipType: string;
+    constructor(name: string, mail: string, membershipDate: Date, membershipType: string) {
+        this.name = name;
+        this.mail = mail;
+        this.membershipDate = membershipDate;
+        this.membershipType = membershipType;
+    }
+}
